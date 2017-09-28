8008e6490fe23701e39a9d8bc44c50c95ea3529c
JaredTan <jared.e.tan@gmail.com>
2017-09-28 15:20:55 -0700

    webcam!

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,29 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+const getVideo = function() {
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(media => {
+      video.src = window.URL.createObjectURL(media);
+      video.play;
+    })
+    .catch(err => {
+      console.error(err);
+    })
+}
+
+const paintToCanvas = function() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval( () => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 5)
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
